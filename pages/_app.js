7d7f60d00784238c8828efb4554fffbde1572801
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2023-09-07 14:38:00 +0100

    new pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "../styles/accordion.css";
+import "../styles/carousel.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;

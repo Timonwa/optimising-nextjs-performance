899844a2ae072f8e47e892145ac506b68cc811c5
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2023-09-08 14:38:59 +0100

    imported swipperjs css to _app.js

--- pages/carousel.jsx
+++ pages/carousel.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
 // import required modules
 import { Pagination, Navigation } from "swiper/modules";
 import styles from "../styles/Home.module.css";
